0f62547d6a11f98653bc219e5807a629d741c2f0
Tapan <tapansonak786@gmail.com>
2023-12-01 19:15:50 +0530

    Updated profile page

--- frontend/src/components/SuggestedPosts.jsx
+++ frontend/src/components/SuggestedPosts.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { URL,IF } from "../url";
-import sampleImage from '../assets/sample.jpg';
+import { URL, IF } from "../url";
+import sampleImage from "../assets/sample.jpg";
 
-
-const SuggestedPosts = ({ className, posts=[] }) => {
+const SuggestedPosts = ({ className, posts = [] }) => {
   return (
     <div
       className={`w-full shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] rounded-lg p-4 ${className}`}
     >
       <h2 className="font-roboto font-medium text-[#0e2436] md:text-xl">
-      Latest Article
+        Latest Article
       </h2>
       <div className="grid gap-y-5 mt-5 md:grid-cols-2 md:gap-x-5 lg:grid-cols-1">
         {posts.map((item) => (
@@ -18,15 +17,12 @@ const SuggestedPosts = ({ className, posts=[] }) => {
             key={item._id}
             className="flex space-x-3 flex-nowrap items-center"
           >
-             <img
+            <img
               className="aspect-square object-cover rounded-lg w-1/5"
-              src={
-                item?.photo
-                  ? IF + item?.photo : sampleImage
-              }
+              src={item?.photo ? IF + item?.photo : sampleImage}
               alt="image"
-            /> 
-            
+            />
+
             <div className="text-sm font-roboto text-dark-hard font-medium">
               <h3 className="text-sm font-roboto text-[#0e2436] font-medium md:text-base lg:text-lg">
                 <Link to={`/blog/${item.slug}`}>{item.title}</Link>
@@ -49,19 +45,19 @@ const SuggestedPosts = ({ className, posts=[] }) => {
         <p className="text-slate-500 text-xs mt-2">
           There is not tags for this post
         </p>
-      ) : (
-        <div className="flex flex-wrap gap-x-2 gap-y-2 mt-4">
+        ) : (
+          <div className="flex flex-wrap gap-x-2 gap-y-2 mt-4">
           {tags.map((item, index) => (
             <Link
-              key={index}
-              to="/"
-              className="inline-block rounded-md px-3 py-1.5 bg-primary font-roboto text-xs text-white md:text-sm"
+            key={index}
+            to="/"
+            className="inline-block rounded-md px-3 py-1.5 bg-primary font-roboto text-xs text-white md:text-sm"
             >
-              {item}
+            {item}
             </Link>
           ))}
-        </div>
-      )} */}
+          </div>
+        )} */}
     </div>
   );
 };
--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -5,6 +5,7 @@ import ProfilePosts from "../components/ProfilePosts";
 import axios from "axios";
 import { URL } from "../url";
 import { useParams } from "react-router-dom";
+import profileImg from "../assets/profileImg.webp";
 
 const UserProfile = () => {
   const { id } = useParams();
@@ -37,23 +38,98 @@ const UserProfile = () => {
   }, [id]);
 
   return (
+    // <div>
+    //   <Navbar />
+    //   <div className="min-h-[80vh] px-8 md:px-[200px] mt-8 flex md:flex-row flex-col-reverse md:items-start items-start">
+    //     <div className="flex flex-col md:w-[70%] w-full mt-8 md:mt-0">
+    //       <h1 className="text-xl font-bold mb-4">User's posts:</h1>
+    //       {posts?.map((p) => (
+    //         <ProfilePosts key={p._id} p={p} />
+    //       ))}
+    //     </div>
+    //     <div className="md:sticky md:top-12  flex justify-start md:justify-end items-start md:w-[30%] w-full md:items-end">
+    //       <div className="flex flex-col space-y-4 items-start">
+    //         <h1 className="text-xl font-bold mb-4">Profile</h1>
+    //         <p className="px-4 py-2 text-gray-500">Username: {username}</p>
+    //         <p className="px-4 py-2 text-gray-500">Email: {email}</p>
+    //       </div>
+    //     </div>
+    //       <div className="flex flex-col space-y-4 items-start">
+    //         <h1 className="text-2xl font-bold mb-4 text-gray-800">Profile</h1>
+    //         <div className="bg-gray-100 p-2 rounded-md hover:shadow-md transition-all duration-300">
+    //           <p className="px-4 py-2 text-gray-700">Username: {username}</p>
+    //         </div>
+    //         <div className="bg-gray-100 p-2 rounded-md hover:shadow-md transition-all duration-300">
+    //           <p className="px-4 py-2 text-gray-700">Email: {email}</p>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <Footer />
+    // </div>
+
+    // <div>
+    //   <Navbar />
+    //   <div className="min-h-[80vh] px-8 md:px-[200px] mt-8 flex md:flex-row flex-col-reverse md:items-start items-start">
+    //     <div className="flex flex-col md:w-[60%] w-full mt-8 md:mt-0">
+    //       <h1 className="text-3xl font-bold mb-6 text-gray-800">
+    //         User's Posts:
+    //       </h1>
+    //       {posts?.map((p) => (
+    //         <ProfilePosts key={p._id} p={p} />
+    //       ))}
+    //     </div>
+    //     <div className="md:sticky md:top-12 p-4 flex flex-col md:items-end items-start md:w-[40%] w-full transition-all duration-300">
+    //       <div className="flex flex-col space-y-4 items-start">
+    //         <h1 className="text-2xl font-bold mb-4 text-gray-800">Profile</h1>
+    //         <div className="bg-gray-100 p-3 rounded-md hover:shadow-md transition-all duration-300">
+    //           <p className="text-lg text-gray-700">Username: {username}</p>
+    //         </div>
+    //         <div className="bg-gray-100 p-3 rounded-md hover:shadow-md transition-all duration-300">
+    //           <p className="text-lg text-gray-700">Email: {email}</p>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+
+    //   <Footer />
+    // </div>
+
     <div>
       <Navbar />
       <div className="min-h-[80vh] px-8 md:px-[200px] mt-8 flex md:flex-row flex-col-reverse md:items-start items-start">
-        <div className="flex flex-col md:w-[70%] w-full mt-8 md:mt-0">
-          <h1 className="text-xl font-bold mb-4">User's posts:</h1>
+        <div className="flex flex-col md:w-[60%] w-full mt-8 md:mt-0 mb-14">
+          <h1 className="text-3xl font-bold mb-6 text-gray-800">
+            User's Posts:
+          </h1>
           {posts?.map((p) => (
             <ProfilePosts key={p._id} p={p} />
           ))}
         </div>
-        <div className="md:sticky md:top-12  flex justify-start md:justify-end items-start md:w-[30%] w-full md:items-end">
-          <div className="flex flex-col space-y-4 items-start">
-            <h1 className="text-xl font-bold mb-4">Profile</h1>
-            <p className="px-4 py-2 text-gray-500">Username: {username}</p>
-            <p className="px-4 py-2 text-gray-500">Email: {email}</p>
+        <div className="md:sticky md:top-12 p-4 flex flex-col md:items-end items-start md:w-[40%] w-full transition-all duration-300">
+          <div className="flex flex-col space-y-4 items-start bg-slate-200 p-8 px-14 py-10 shadow-md rounded-md">
+            <h1 className="flex items-center text-2xl font-bold mb-4 text-gray-800">
+              Profile
+              <img
+                src={profileImg}
+                alt=""
+                className="h-10 w-10 ml-2 object-cover rounded-full"
+              />
+            </h1>
+            <div className="bg-gray-100 p-3 rounded-md hover:shadow-md transition-all duration-300">
+              <p className="text-lg text-gray-700 font-semibold">
+                Username: {username}
+              </p>
+            </div>
+            <div className="bg-gray-100 p-3 rounded-md hover:shadow-md transition-all duration-300">
+              <p className="text-lg text-gray-700 font-semibold">
+                Email: {email}
+              </p>
+            </div>
           </div>
         </div>
       </div>
+
       <Footer />
     </div>
   );
