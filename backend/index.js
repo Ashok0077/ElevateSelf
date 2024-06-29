45106cb802ccc18559f371b7281cb3ec42d304ce
Ashok0077 <vaishnavashok7297@gmail.com>
2024-06-29 10:44:01 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,7 @@ const connectDB=async()=>{
 
 
 //middlewares
+app.use(express.static('public'));
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
