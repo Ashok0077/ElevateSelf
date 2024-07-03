91ed44765963363a1a445dc6321850c6110bdab8
Ashok0077 <vaishnavashok7297@gmail.com>
2024-07-03 12:14:37 +0530

    commit

--- backend/verifyToken.js
+++ backend/verifyToken.js
@@ -2,10 +2,11 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
-
+  
+  consile.log(token);
   
   if (!token) {
-    return res.status(401).json({message : "Can't able to get token, check if you are login or not ", token : token});
+    return res.status(401).json("you are not autherized!");
   }
 
   jwt.verify(token, process.env.SECRET, async (err, data) => {
