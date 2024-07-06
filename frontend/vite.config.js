ab3c0f5ca3a09900b9a2a6932b8b53d890e1520d
Ashok0077 <vaishnavashok7297@gmail.com>
2024-07-06 16:19:41 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,53 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        target: "https://elevate-self-api-peach.vercel.app",
-         //target: "https://elevate-self-backend.vercel.app",
+      '/api': {
+        target: 'https://elevate-self-api-peach.vercel.app',
         secure: false,
       },
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'ElevateSelf',
+        short_name: 'ElevateSelf',
+        description: 'Web site created using Vite',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'icons/ElevateSelf.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/ElevateSelf.png',
+            sizes: '256x256',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/ElevateSelf.png',
+            sizes: '384x384',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/ElevateSelf.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff'
+      },
+      registerType: 'autoUpdate',
+      includeAssets: ['ElevateSelf.ico', 'robots.txt', 'icons/ElevateSelf.png']
+    })
+  ],
 });
